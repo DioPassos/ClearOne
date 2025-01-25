326572d0f41804a8582b39ac0f21a24d79c7874f
@dioelitonpassos <dioelitonpassos@hotmail.com>
2025-01-25 13:17:32 -0300

    incluindo while

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 alert ('Bem vindo ao Jog do número secreto')
 let numeroSecreto=10;
 console.log(numeroSecreto)
-let chute = prompt('escolha um numero entre 1 e 30')
+let chute 
 
-if (chute==numeroSecreto){
-    alert(`você acertou o número secreto ${numeroSecreto}`);
 
-} else{
-    if (chute> numeroSecreto){
-        alert(`tente novamente,o numero secreto é menor que o ${chute}`)
-    }else{
-        alert(`o numero secreto é maior que ${chute}`)
-
-    
+while(chute != numeroSecreto){
+    chute = prompt('escolha um numero entre 1 e 30')
+    if (chute==numeroSecreto){
+         alert(`você acertou o número secreto ${numeroSecreto}`);
+        
+    } else {
+        if (chute> numeroSecreto){
+            alert(`tente novamente,o numero secreto é menor que o ${chute}`)
+        }else {
+            alert(`o numero secreto é maior que ${chute}`)
+        
+            
+        }
+        
     }
-
 }
-    
-
