d397cabda678ac2a478bbd9883c138fd4477a0f4
@dioelitonpassos <dioelitonpassos@hotmail.com>
2025-01-24 21:10:52 -0300

    codigo2

---
+++ app.js
@@ -0,0 +1,8 @@
+alert ('Estudo js')
+let numeroSecreto=20;
+let chute = prompt('escolha um numero entre 1 e 30')
+
+if (numeroSecreto==chute){
+    console.log('você acertou o número secreto')
+    
+}
