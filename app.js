ec35c042b7a28e7c361c811100c2ac793b5b221c
@dioelitonpassos <dioelitonpassos@hotmail.com>
2025-01-24 21:20:40 -0300

    msg

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-alert ('Estudo js')
+alert ('Bem vindo ao Jog do número secreto')
 let numeroSecreto=20;
 let chute = prompt('escolha um numero entre 1 e 30')
 
-if (numeroSecreto==chute){
+if (chute==numeroSecreto){
     console.log('você acertou o número secreto')
-    
+
+} else{
+    alert("tente novemente,chute foi errado")
 }
