71726565e26bea77edd4ea0e3e0849ed66e54056
@dioelitonpassos <dioelitonpassos@hotmail.com>
2025-01-25 13:23:27 -0300

    adicionando tentativas

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 alert ('Bem vindo ao Jog do número secreto')
 let numeroSecreto=10;
-console.log(numeroSecreto)
-let chute 
+console.log(numeroSecreto);
+let chute ;
+let tentativas = 1;
 
 
 while(chute != numeroSecreto){
     chute = prompt('escolha um numero entre 1 e 30')
     if (chute==numeroSecreto){
-         alert(`você acertou o número secreto ${numeroSecreto}`);
+         alert(`você acertou o número secreto é ${numeroSecreto}`);
         
     } else {
         if (chute> numeroSecreto){
-            alert(`tente novamente,o numero secreto é menor que o ${chute}`)
+            alert(`tente novamente,o numero secreto é menor que o ${chute}`);
         }else {
-            alert(`o numero secreto é maior que ${chute}`)
+            alert(`o numero secreto é maior que ${chute}`);
         
             
         }
-        
+        tentativas ++
     }
 }
